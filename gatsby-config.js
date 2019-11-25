b129dd78b13b172ab9e9a200c0b6516187984dc8
Stefan Imhoff <stefan@imhoff.name>
2019-11-25 20:52:27 +0100

    chore(gatsby): create basic Gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Stefan Imhoff - Developer/Designer from Hamburg, Germany',
+    description: 'Front-End Web Developer / Designer / Minimalist / Introvert / Japanophile',
+    author: 'Stefan Imhoff',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+  ],
+}
