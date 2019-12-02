b6a6203a23613fc4b4fcd21ca1f54e38150f7161
Stefan Imhoff <stefan@imhoff.name>
2019-12-02 21:23:41 +0100

    chore(eslint): add configuration and scripts for ESLint

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,7 +33,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
