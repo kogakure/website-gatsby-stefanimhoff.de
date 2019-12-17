666ccb700d4f11c7e788111193d14e8e3438982a
Stefan Imhoff <stefan@imhoff.name>
2019-12-17 19:42:46 +0100

    chore(stylelint): add stylelint-prettier, remove processor and fix styles

--- src/global/Layout/Layout.styles.tsx
+++ src/global/Layout/Layout.styles.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const MainLayout = styled.main`
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: 3fr 1fr;
   max-width: 90%;
   margin: 1rem auto;
-  display: grid;
-  grid-template-columns: 3fr 1fr;
-  grid-gap: 4rem;
 `;
 
 export const Styled = {
