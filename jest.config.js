2392fa47e3b977b5559093ca14bb5c63b81c3ac3
Stefan Imhoff <stefan@imhoff.name>
2019-12-18 16:40:24 +0100

    chore(jest): add typeahead plugins and run tests before commit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
+  rootDir: path.join(__dirname),
   preset: 'jest-preset-gatsby/typescript',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   collectCoverageFrom: ['**/src/**/*.{js,ts,tsx}'],
@@ -10,4 +13,8 @@ module.exports = {
       lines: 30,
     },
   },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
