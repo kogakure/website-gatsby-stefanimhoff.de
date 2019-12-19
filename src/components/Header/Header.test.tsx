6167e9a9d28225c7c4aec1098f5d13ade61f9231
Stefan Imhoff <stefan@imhoff.name>
2019-12-19 14:41:50 +0100

    chore(jest): add support for jest-axe

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { axe } from 'jest-axe';
 
 import { render } from '../../utils/test-utils';
 
 import { Header } from './Header';
 
 describe('Header', () => {
-  it('renders correctly', () => {
+  test('renders correctly', () => {
     const { container } = render(<Header />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('renders correctly with prop siteTitle', () => {
+  test('renders correctly with prop siteTitle', () => {
     const siteTitle = 'I am the title of the site';
     const { container } = render(<Header siteTitle={siteTitle} />);
 
     expect(container.firstChild).toMatchSnapshot();
     expect(container).toHaveTextContent(siteTitle);
+  });
+
+  test('is accessible', async () => {
+    const siteTitle = 'I am an accessible Title';
+    const { container } = render(<Header siteTitle={siteTitle} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 });
