8b1ec14683d7f426ea6364c950cfeb25ea210150
Stefan Imhoff <stefan@imhoff.name>
2020-01-04 15:06:42 +0100

    fix: remove property from type definition

--- src/typings/PostPageContext.d.ts
+++ src/typings/PostPageContext.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface PostPageContextData {
-  isCreatedByStatefulCreatePages: boolean;
   next?: any;
   previous?: any;
   slug: string;
