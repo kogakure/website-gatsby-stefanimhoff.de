f996bb95421685bd2ce38c719dfa5a5828403475
Stefan Imhoff <stefan@imhoff.name>
2020-01-16 19:28:46 +0100

    refactor: move EmojifyProvider into gatsby-browser and gatsby-ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 
+import { EmojifyProvider } from './src/contexts';
 import { GlobalStyles } from './src/components/Layout/GlobalStyles';
 import { lightTheme } from './src/themes';
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={lightTheme}>
-    <>
+    <EmojifyProvider>
       <Normalize />
       <GlobalStyles />
       {element}
-    </>
+    </EmojifyProvider>
   </ThemeProvider>
 );
 
