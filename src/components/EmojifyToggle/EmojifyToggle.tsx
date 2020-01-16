de5c17ee0371c99c3a6d6752d061e0ac7783653e
Stefan Imhoff <stefan@imhoff.name>
2020-01-16 21:09:05 +0100

    test: add tests for EmojifyToggle

--- src/components/EmojifyToggle/EmojifyToggle.tsx
+++ src/components/EmojifyToggle/EmojifyToggle.tsx
@@ -3,12 +3,15 @@ import * as React from 'react';
 import { EmojifyContext } from '../../contexts';
 
 const EmojifyToggle: React.FC = () => {
-  const { emojified, setEmojified } = React.useContext(EmojifyContext);
-  const buttonText = emojified ? '🙈' : '🐵';
+  const { emojified, toggleEmojifyValue } = React.useContext(EmojifyContext);
+  const buttonImage = emojified ? '🙈' : '🐵';
+  const buttonText = emojified ? 'Hide Emoji' : 'Show Emoji';
 
   return (
-    <button type="button" onClick={() => setEmojified(!emojified)}>
-      {buttonText}
+    <button type="button" onClick={toggleEmojifyValue}>
+      <span role="img" aria-label={buttonText}>
+        {buttonImage}
+      </span>
     </button>
   );
 };
--- src/contexts/EmojifyContext.tsx
+++ src/contexts/EmojifyContext.tsx
@@ -1,18 +1,30 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React, { useState } from 'react';
 
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
 const initialState = {
   emojified: false,
+  toggleEmojifyValue: () => {},
   setEmojified: (emojified: boolean) => {},
 };
 
 const EmojifyContext = React.createContext(initialState);
 
 const EmojifyProvider: React.FC = ({ children }) => {
-  const [emojified, setEmojified] = useState(false);
+  const [value, setValue] = useLocalStorage('emojify', '🚫');
+  const initialValue = value !== '🚫';
+  const [emojified, setEmojified] = useState(initialValue);
+
+  const toggleEmojifyValue = () => {
+    setEmojified(!emojified);
+    setValue(emojified ? '🚫' : '✅');
+  };
 
   return (
-    <EmojifyContext.Provider value={{ emojified, setEmojified }}>
+    <EmojifyContext.Provider
+      value={{ emojified, setEmojified, toggleEmojifyValue }}
+    >
       {children}
     </EmojifyContext.Provider>
   );
