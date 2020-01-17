bc221e4d5aebc94b81474f300442f58d20f62d98
Stefan Imhoff <stefan@imhoff.name>
2020-01-17 20:50:38 +0100

    feat: create LocalizedDate component to render correct date

---
+++ src/components/LocalizedDate/LocalizedDate.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const LocalizedDateContainer = styled.time`
+  cursor: pointer;
+`;
+
+const Kanji = styled.span``;
+
+export const Styled = {
+  LocalizedDateContainer,
+  Kanji,
+};
