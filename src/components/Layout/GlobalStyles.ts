930342eacf02ac2816c3195da8cc898fe5db374a
Stefan Imhoff <stefan@imhoff.name>
2020-01-19 15:47:48 +0100

    feat(colorswatch): add new ColorSwatch component

--- src/components/Layout/GlobalStyles.ts
+++ src/components/Layout/GlobalStyles.ts
@@ -7,19 +7,26 @@ import globalCSS from './styles/global.css';
 import highlightCodeLine from './styles/highlight-code-line.css';
 
 const GlobalStyles = createGlobalStyle<{ theme: ThemeType & ThemeProps }>`
+  /* stylelint-disable plugin/stylelint-no-indistinguishable-colors */
   ${globalCSS}
   ${highlightCodeLine}
 
   .light {
     --colorPrimary: ${lightColors.primary};
     --colorBackground: ${lightColors.background};
+    --colorBackgroundFull: #FFF;
+    --colorTextFull: #000;
+    --colorBorder: rgba(0, 0, 0, 0.2);
     --opacityLight: 0;
     --opacityDark: 1;
   }
 
   .light .green {
-    --colorPrimary: black;
+    --colorPrimary: darkgrey;
     --colorBackground: lightgreen;
+    --colorBackgroundFull: #FFF;
+    --colorTextFull: #000;
+    --colorBorder: rgba(0, 0, 0, 0.2);
     --opacityLight: 0;
     --opacityDark: 1;
   }
@@ -27,13 +34,19 @@ const GlobalStyles = createGlobalStyle<{ theme: ThemeType & ThemeProps }>`
   .dark {
     --colorPrimary: ${darkColors.primary};
     --colorBackground: ${darkColors.background};
+    --colorBackgroundFull: #000;
+    --colorTextFull: #FFF;
+    --colorBorder: rgba(255, 255, 255, 0.2);
     --opacityLight: 1;
     --opacityDark: 0;
   }
 
   .dark .green {
-    --colorPrimary: black;
+    --colorPrimary: darkgrey;
     --colorBackground: darkgreen;
+    --colorBackgroundFull: #000;
+    --colorTextFull: #FFF;
+    --colorBorder: rgba(255, 255, 255, 0.2);
     --opacityLight: 1;
     --opacityDark: 0;
   }
