553c87b3e46d3e9d1897da5233351d1539427944
Stefan Imhoff <stefan@imhoff.name>
2020-01-19 15:46:52 +0100

    chore(svg): add information icon

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,3 +1,4 @@
 export { default as ArrowUp } from './ArrowUp';
+export { default as Information } from './Information';
 export { default as Moon } from './Moon';
 export { default as Sun } from './Sun';
