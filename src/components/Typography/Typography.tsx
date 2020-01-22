8509ca3164e8b954b54c1d6e64718a53f8b73136
Stefan Imhoff <stefan@imhoff.name>
2020-01-22 08:35:22 +0100

    test(typography): add snapshot tests for Title, Headline, Subline and Paragraph

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -8,24 +8,16 @@ const Title: React.FC<StyledSystemProps> = ({ children, ...props }) => (
   <DynamicComponent {...props}>{children}</DynamicComponent>
 );
 
-const Headline: React.FC<StyledSystemProps> = ({ children, ...props }) => (
-  <DynamicComponent {...props}>{children}</DynamicComponent>
-);
-
-const Subline: React.FC<StyledSystemProps> = ({ children, ...props }) => (
-  <DynamicComponent {...props}>{children}</DynamicComponent>
-);
-
-const Paragraph: React.FC<StyledSystemProps> = ({ children, ...props }) => (
-  <DynamicComponent {...props}>{children}</DynamicComponent>
-);
-
 Title.defaultProps = {
   as: 'h1',
   fontSize: 5,
   fontWeight: 900,
   letterSpacing: 'normal',
 };
+
+const Headline: React.FC<StyledSystemProps> = ({ children, ...props }) => (
+  <DynamicComponent {...props}>{children}</DynamicComponent>
+);
 
 Headline.defaultProps = {
   as: 'h2',
@@ -34,12 +26,20 @@ Headline.defaultProps = {
   letterSpacing: 'normal',
 };
 
+const Subline: React.FC<StyledSystemProps> = ({ children, ...props }) => (
+  <DynamicComponent {...props}>{children}</DynamicComponent>
+);
+
 Subline.defaultProps = {
   as: 'h3',
   fontSize: 3,
   fontWeight: 900,
   letterSpacing: 'normal',
 };
+
+const Paragraph: React.FC<StyledSystemProps> = ({ children, ...props }) => (
+  <DynamicComponent {...props}>{children}</DynamicComponent>
+);
 
 Paragraph.defaultProps = {
   as: 'p',
