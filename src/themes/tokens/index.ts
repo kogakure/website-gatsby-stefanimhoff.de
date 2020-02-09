48be7675e670499e0f2938b90901ff90b8aff0af
Stefan Imhoff <stefan@imhoff.name>
2020-02-09 17:45:51 +0100

    feat(token): add radii tokens

--- src/themes/tokens/index.ts
+++ src/themes/tokens/index.ts
@@ -2,5 +2,6 @@ export { breakpoints, mediaQueries } from './breakpoints';
 export { fontWeights } from './fontWeights';
 export { lightColors, darkColors } from './colors';
 export { lineHeights } from './lineHeights';
+export { radii } from './radii';
 export { space } from './space';
 export { typographicScale } from './typographicScale';
---
+++ src/themes/tokens/radii.ts
@@ -0,0 +1,10 @@
+export interface Radii {
+  [key: number]: string;
+}
+
+export const radii: Radii = {
+  0: '0',
+  1: '2px',
+  2: '4px',
+  3: '8px',
+};
