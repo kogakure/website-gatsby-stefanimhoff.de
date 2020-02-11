b5035b598f2c26efb1a496be643939101aa74521
Stefan Imhoff <stefan@imhoff.name>
2020-02-11 19:43:46 +0100

    chore: clean up temporary layout CSS

--- src/components/Layout/Layout.styles.tsx
+++ src/components/Layout/Layout.styles.tsx
@@ -2,11 +2,9 @@ import styled from 'styled-components';
 
 const MainLayout = styled.main`
   background: var(--colorBackground);
-  display: grid;
-  grid-gap: 4rem;
-  grid-template-columns: 3fr 1fr;
   margin: 1rem auto;
-  max-width: 90%;
+  padding-left: 2rem;
+  padding-right: 2rem;
 `;
 
 export const Styled = {
