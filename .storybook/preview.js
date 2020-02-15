60d1e039c980c7d7ffb3bd31287be441c0ef16a8
Stefan Imhoff <stefan@imhoff.name>
2020-02-15 19:45:57 +0100

    chore(storybook): add setup and configuration for Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import { configure, addDecorator } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withOptions } from '@storybook/addon-options';
+
+import { EmojifyProvider, LocalizedDateProvider } from '../src/contexts';
+import { lightTheme } from '../src/themes';
+import { GlobalStyles } from '../src/components/Layout/GlobalStyles';
+import { ThemeToggle } from '../src/components/ThemeToggle';
+import { EmojifyToggle } from '../src/components/EmojifyToggle';
+
+import '../src/utils/theme';
+
+const Grid = styled.div`
+  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 50px 1fr;
+  height: 100vh;
+  justify-items: center;
+`;
+
+const Decorator = storyFn => (
+  <ThemeProvider theme={lightTheme}>
+    <EmojifyProvider>
+      <LocalizedDateProvider>
+        <Normalize />
+        <GlobalStyles />
+        <Grid>
+          <nav>
+            <ThemeToggle />
+            <EmojifyToggle />
+          </nav>
+          <section>{storyFn()}</section>
+        </Grid>
+      </LocalizedDateProvider>
+    </EmojifyProvider>
+  </ThemeProvider>
+);
+
+addDecorator(Decorator);
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+
+global.__PATH_PREFIX__ = '';
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname);
+};
+
+addDecorator(
+  withOptions({
+    name: 'stefanimhoff.de',
+    url: 'https://www.stefanimhoff.de/',
+    hierarchySeparator: /\/|\./,
+    hierarchyRootSeparator: /\|/,
+  })
+);
+
+configure(
+  require.context('../src/components', true, /\.stories\.tsx$/),
+  module
+);
