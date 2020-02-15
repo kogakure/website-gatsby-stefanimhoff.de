12d5c2b3ae31d2be9e90b9aa073cfeff729f2a6f
Stefan Imhoff <stefan@imhoff.name>
2020-02-15 09:53:36 +0100

    refactor(text-link): to have font-weight bold

--- src/components/TextLink/TextLink.styles.tsx
+++ src/components/TextLink/TextLink.styles.tsx
@@ -1,9 +1,10 @@
 import { Link as GatsbyLink } from 'gatsby';
 import styled, { css } from 'styled-components';
-import { fontSize } from 'styled-system';
+import { fontSize, fontWeight } from 'styled-system';
 
 const linkStyles = css`
   ${fontSize};
+  ${fontWeight};
   color: var(--colorText);
   text-decoration-color: var(--colorLink);
   text-decoration-thickness: 0.1em;
