71110d4cb10760101fd9efefc442e566e0f7c433
Stefan Imhoff <stefan@imhoff.name>
2020-03-22 16:13:06 +0100

    test(inline-quote): add stories and tests

---
+++ src/components/InlineQuote/InlineQuote.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { axe } from 'jest-axe';
+
+import { render } from '../../utils/test-utils';
+
+import {
+  Default,
+  GermanNested,
+  JapaneseNested,
+  Nested,
+} from './InlineQuote.stories';
+
+describe('InlineQuote', () => {
+  test('renders correctly', () => {
+    const { container } = render(<Default />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('is accessible', async () => {
+    const { container } = render(<Default />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  describe('nested', () => {
+    test('renders correctly', () => {
+      const { container } = render(<Nested />);
+
+      expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test('is accessible', async () => {
+      const { container } = render(<Nested />);
+      const results = await axe(container);
+
+      expect(results).toHaveNoViolations();
+    });
+    describe('in German', () => {
+      test('renders correctly', () => {
+        const { container } = render(<GermanNested />);
+
+        expect(container.firstChild).toMatchSnapshot();
+      });
+
+      test('is accessible', async () => {
+        const { container } = render(<GermanNested />);
+        const results = await axe(container);
+
+        expect(results).toHaveNoViolations();
+      });
+    });
+
+    describe('in Japanese', () => {
+      test('renders correctly', () => {
+        const { container } = render(<JapaneseNested />);
+
+        expect(container.firstChild).toMatchSnapshot();
+      });
+
+      test('is accessible', async () => {
+        const { container } = render(<JapaneseNested />);
+        const results = await axe(container);
+
+        expect(results).toHaveNoViolations();
+      });
+    });
+  });
+});
