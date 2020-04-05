e8b9ec454596fcf2c07672398f9d65edb20e7a58
Stefan Imhoff <stefan@imhoff.name>
2020-04-05 14:45:15 +0200

    feat(spacer): add component and styling

---
+++ src/components/Spacer/Spacer.styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import {
+  border,
+  padding,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+  paddingTop,
+} from 'styled-system';
+
+const Spacer = styled.div`
+  ${border}
+  ${paddingBottom}
+  ${paddingLeft}
+  ${paddingRight}
+  ${paddingTop}
+  ${padding}
+`;
+
+export const Styled = {
+  Spacer,
+};
