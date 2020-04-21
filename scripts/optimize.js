253a19bcb1a2957aac4d14bf5f0bfd7995184836
Stefan Imhoff <stefan@imhoff.name>
2020-04-21 17:45:32 +0200

    chore: add script to preoptimize images

---
+++ scripts/optimize.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const sharp = require('sharp');
+const glob = require('glob');
+const fs = require('fs-extra');
+
+const matches = glob.sync('content/assets/images/**/*.{png,jpg,jpeg}');
+
+const MAX_WIDTH = 1800;
+const QUALITY = 80;
+
+Promise.all(
+  matches.map(async (match) => {
+    const stream = sharp(match);
+    const info = await stream.metadata();
+    if (info.width < MAX_WIDTH) {
+      return;
+    }
+    const optimizedName = match.replace(
+      /(\..+)$/,
+      (_match, ext) => `-optimized${ext}`
+    );
+
+    await stream
+      .resize(MAX_WIDTH)
+      .jpeg({ quality: QUALITY })
+      .toFile(optimizedName);
+
+    fs.rename(optimizedName, match);
+  })
+);
