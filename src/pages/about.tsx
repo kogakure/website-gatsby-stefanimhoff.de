0006e75e75d3b5656ed55430ab879f0a15f5a451
Stefan Imhoff <stefan@imhoff.name>
2020-05-25 20:14:31 +0200

    chore: add empty placeholders for some pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Layout } from '../layout';
+
+const AboutPage = () => <Layout>About</Layout>;
+
+export default AboutPage;
---
+++ src/pages/journal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Layout } from '../layout';
+
+const JournalPage = () => <Layout>Journal</Layout>;
+
+export default JournalPage;
