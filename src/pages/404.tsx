559b20cbdd728ea241cd8304eb18470e59173157
Stefan Imhoff <stefan@imhoff.name>
2020-05-26 14:34:26 +0200

    feat(404): add 404 page

--- src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Layout } from '../layout';
-
-const NotFoundPage = () => <Layout>404</Layout>;
-
-export default NotFoundPage;
---
+++ src/screens/404/404.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+import {
+  fontSize,
+  fontWeight,
+  lineHeight,
+  mediaQuery,
+} from '../../layout/tokens';
+import { Title } from '../../components/typography';
+
+export const Title404 = styled(Title)`
+  grid-column: 1 / -1;
+  grid-row: 1;
+  margin-bottom: 0;
+  z-index: 1;
+
+  @media (${mediaQuery.desktop}) {
+    grid-column: 1 / -8;
+  }
+`;
+
+export const Haiku = styled.div`
+  grid-column: 1 / -1;
+  grid-row: 2;
+  z-index: 1;
+
+  @media (${mediaQuery.tablet}) {
+    grid-column: 3 / -6;
+  }
+`;
+
+export const Huge404 = styled.span`
+  color: var(--colorForegroundFeather);
+  font-size: ${fontSize[9]};
+  font-weight: ${fontWeight.black};
+  grid-column: -6 / -1;
+  grid-row: 2 / 4;
+  line-height: ${lineHeight[1]};
+  text-align: right;
+  writing-mode: vertical-lr;
+  z-index: 0;
+
+  @media (${mediaQuery.desktop}) {
+    grid-column: -6 / -2;
+    grid-row: 1 / 4;
+  }
+`;
