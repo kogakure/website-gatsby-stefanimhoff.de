3989cced47394859354b61e4a31f173ae01d802c
Stefan Imhoff <stefan@imhoff.name>
2020-05-26 14:03:03 +0200

    chore(plop): move template folder into scripts folder

--- plopfile.js
+++ plopfile.js
@@ -2,37 +2,9 @@
 const currentDir = process.cwd();
 const currentDate = new Date();
 
+const templatePath = 'scripts/templates';
+
 module.exports = function (plop) {
-  plop.setGenerator('component', {
-    description: 'Create a new functional component',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Component Name',
-        validate(value) {
-          if (/.+/.test(value)) {
-            return true;
-          }
-          return 'Component Name is required';
-        },
-      },
-    ],
-    actions() {
-      process.chdir(plop.getPlopfilePath());
-
-      return [
-        {
-          type: 'addMany',
-          destination: `${currentDir}/src/components/{{dashCase name}}`,
-          base: 'templates/component',
-          templateFiles: '**/*.txt',
-          stripExtensions: ['txt'],
-        },
-      ];
-    },
-  });
-
   plop.setGenerator('post', {
     description: 'Create a new blog post',
     prompts: [
@@ -55,12 +27,42 @@ module.exports = function (plop) {
         {
           type: 'addMany',
           destination: `${currentDir}/content/posts/${currentDate.getFullYear()}/{{dashCase title}}`,
-          base: 'templates/blog-post',
+          base: `${templatePath}/blog-post`,
           templateFiles: '**/*.txt',
           stripExtensions: ['txt'],
           data: {
             date: currentDate.toISOString(),
           },
+        },
+      ];
+    },
+  });
+
+  plop.setGenerator('component', {
+    description: 'Create a new functional component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component Name',
+        validate(value) {
+          if (/.+/.test(value)) {
+            return true;
+          }
+          return 'Component Name is required';
+        },
+      },
+    ],
+    actions() {
+      process.chdir(plop.getPlopfilePath());
+
+      return [
+        {
+          type: 'addMany',
+          destination: `${currentDir}/src/components/{{dashCase name}}`,
+          base: `${templatePath}/component`,
+          templateFiles: '**/*.txt',
+          stripExtensions: ['txt'],
         },
       ];
     },
