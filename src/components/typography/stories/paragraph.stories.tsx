de9d2937a3219591de5b9d60f3c2a5dcfb71860c
Stefan Imhoff <stefan@imhoff.name>
2020-05-27 15:34:54 +0200

    improvement(typography): add property to remove margin to components

--- src/components/typography/stories/paragraph.stories.tsx
+++ src/components/typography/stories/paragraph.stories.tsx
@@ -18,3 +18,16 @@ export const Default = () => (
     helvetica blog, paleo cardigan seitan kombucha normcore.
   </Paragraph>
 );
+
+export const NoMargin = () => (
+  <Paragraph noMargin>
+    Lorem ipsum dolor amet shoreditch tbh butcher 90's portland, keytar street
+    art. Pork belly you probably haven't heard of them ethical post-ironic
+    hella, cray kale chips fashion axe chillwave shabby chic man bun slow-carb
+    unicorn. Live-edge intelligentsia tumblr quinoa man braid. Hexagon disrupt
+    echo park brunch direct trade four dollar toast chartreuse, drinking vinegar
+    wolf bushwick pork belly. Vexillologist sartorial literally venmo blog.
+    Tumblr you probably haven't heard of them bitters swag waistcoat af. Deep v
+    helvetica blog, paleo cardigan seitan kombucha normcore.
+  </Paragraph>
+);
--- src/components/typography/stories/subheadline.stories.tsx
+++ src/components/typography/stories/subheadline.stories.tsx
@@ -7,3 +7,5 @@ export default {
 };
 
 export const Default = () => <Subheadline>Subheadline</Subheadline>;
+
+export const NoMargin = () => <Subheadline noMargin>Subheadline</Subheadline>;
