5297820a09addb79a9025be438730d3c2b052428
Stefan Imhoff <stefan@imhoff.name>
2020-05-27 13:02:27 +0200

    improvement(templates): allow all templates to receive the layout variant

--- src/templates/regular.tsx
+++ src/templates/regular.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Column, Layout, MDXProviderContainer } from '../layout';
+import { Column, Layout, LayoutProps, MDXProviderContainer } from '../layout';
 
-const RegularTemplate: React.FC = ({ children }) => (
-  <Layout>
+const RegularTemplate: React.FC<LayoutProps> = ({ variant, children }) => (
+  <Layout variant={variant}>
     <MDXProviderContainer>
       <Column variant="regular">{children}</Column>
     </MDXProviderContainer>
