f6adacfc2e3fce68049992ef18fe7fe24b8aa66f
Stefan Imhoff <stefan@imhoff.name>
2020-05-27 12:45:15 +0200

    improvement(verse): add possibility to left align or center align the verse

--- src/components/verse/verse.stories.tsx
+++ src/components/verse/verse.stories.tsx
@@ -20,3 +20,31 @@ export const Default = () => (
     </p>
   </Verse>
 );
+
+export const Center = () => (
+  <Verse variant="center">
+    <p>
+      God guard me from those thoughts men think
+      <br />
+      In the mind alone;
+      <br />
+      He that sings a lasting song
+      <br />
+      Thinks in a marrow bone.
+    </p>
+  </Verse>
+);
+
+export const Left = () => (
+  <Verse variant="left">
+    <p>
+      God guard me from those thoughts men think
+      <br />
+      In the mind alone;
+      <br />
+      He that sings a lasting song
+      <br />
+      Thinks in a marrow bone.
+    </p>
+  </Verse>
+);
--- src/components/verse/verse.styles.tsx
+++ src/components/verse/verse.styles.tsx
@@ -1,22 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { space } from '../../layout/tokens';
 
-const Verse = styled.blockquote`
+import { VerseProps } from './verse';
+
+export const variants = {
+  center: {
+    blockquote: css`
+      margin: ${space[10]};
+    `,
+    pre: css`
+      margin-left: auto;
+      margin-right: auto;
+    `,
+  },
+  left: {
+    blockquote: css`
+      margin-bottom: ${space[10]};
+      margin-left: 0;
+      margin-right: 0;
+      margin-top: ${space[10]};
+    `,
+    pre: null,
+  },
+};
+
+const Verse = styled.blockquote<VerseProps>`
+  ${({ variant }) => variant && variants[variant].blockquote}
   display: flex;
   font-style: italic;
-  margin: ${space[10]};
 
   p {
     margin: 0;
   }
 `;
 
-const Preformatted = styled.pre`
+const Preformatted = styled.pre<VerseProps>`
+  ${({ variant }) => variant && variants[variant].pre}
   font-family: inherit;
   margin-bottom: 0;
-  margin-left: auto;
-  margin-right: auto;
   margin-top: 0;
 `;
 
