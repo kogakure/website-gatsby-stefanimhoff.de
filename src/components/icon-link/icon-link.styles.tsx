bfa8fded101be8653a51803b52c09653b5a27b9c
Stefan Imhoff <stefan@imhoff.name>
2020-05-31 22:01:16 +0200

    refactor: allow IconLink to have animations

--- src/components/icon-link/icon-link.styles.tsx
+++ src/components/icon-link/icon-link.styles.tsx
@@ -1,11 +1,34 @@
-import styled from 'styled-components';
+/* eslint-disable @typescript-eslint/indent */
+import styled, { css } from 'styled-components';
 
-const Icon = styled.span`
+import { transitionDuration } from '../../layout/tokens';
+
+import { AnimationDirection } from '.';
+
+const Icon = styled.span<{ animation: AnimationDirection }>`
   display: inline-flex;
   fill: var(--colorForeground);
   margin-left: 0.2em;
   position: relative;
   top: 0.2em;
+  transition-duration: ${transitionDuration[5]};
+  transition-property: transform;
+  transition-timing-function: ease-in-out;
+
+  a:hover &,
+  a:active &,
+  a:focus & {
+    ${({ animation }) =>
+      animation === 'right' &&
+      css`
+        transform: translate3D(0.25rem, 0, 0);
+      `}
+    ${({ animation }) =>
+      animation === 'down' &&
+      css`
+        transform: translate3D(0, 0.25rem, 0);
+      `}
+  }
 `;
 
 export const Styled = {
--- src/components/icon-link/icon-link.tsx
+++ src/components/icon-link/icon-link.tsx
@@ -4,18 +4,21 @@ import { TextLink, TextLinkAnchorProps } from '../text-link';
 
 import { Styled } from './icon-link.styles';
 
+export type AnimationDirection = 'right' | 'down' | null;
 export type IconLinkProps = {
   icon: React.ReactElement;
+  animation?: AnimationDirection;
 };
 export type TextLinkWithIconProps = IconLinkProps & TextLinkAnchorProps;
 
 export const IconLink: React.FC<TextLinkWithIconProps> = ({
+  animation = null,
   children,
   icon,
   ...props
 }) => (
   <TextLink {...props}>
     {children}
-    <Styled.Icon>{icon}</Styled.Icon>
+    <Styled.Icon animation={animation}>{icon}</Styled.Icon>
   </TextLink>
 );
