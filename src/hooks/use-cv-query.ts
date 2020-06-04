8d429c8bdadf0f6aad2d48b93f5a435a42e80889
Stefan Imhoff <stefan@imhoff.name>
2020-06-04 19:18:11 +0200

    chore(cv): add data file and print content to page

---
+++ src/hooks/use-cv-query.ts
@@ -0,0 +1,76 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+import { CvQueryData } from '../typings/graphql';
+
+export const useCvQuery = () => {
+  const { allCvYaml }: CvQueryData = useStaticQuery(graphql`
+    query CVQuery {
+      allCvYaml {
+        nodes {
+          personal {
+            name
+            tagline
+          }
+          summary {
+            headline
+            text
+          }
+          experience {
+            headline
+            items {
+              company
+              duration
+              location
+              tasks
+              title
+              url
+            }
+          }
+          education {
+            headline
+            items {
+              degree
+              duration
+              location
+              school
+              url
+            }
+          }
+          skills {
+            headline
+            items {
+              headline
+              tags
+            }
+          }
+          languages {
+            headline
+            items {
+              language
+              level
+            }
+          }
+          awards {
+            headline
+            items
+          }
+          interests {
+            headline
+            items
+          }
+          projects {
+            headline
+            items {
+              description
+              notes
+              title
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return { allCvYaml };
+};
