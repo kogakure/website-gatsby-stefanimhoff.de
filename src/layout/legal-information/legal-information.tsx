a416c449f027a5c984831da1b986d0a57db9d906
Stefan Imhoff <stefan@imhoff.name>
2020-06-06 16:36:07 +0200

    chore: replace separator with slash

--- src/layout/legal-information/legal-information.tsx
+++ src/layout/legal-information/legal-information.tsx
@@ -9,7 +9,7 @@ export const LegalInformation: React.FC = ({ ...props }) => (
       <Styled.Author>
         <Styled.AuthorLink to="/imprint/">Stefan Imhoff</Styled.AuthorLink>
       </Styled.Author>
-      <Styled.Bullet> • </Styled.Bullet>
+      <Styled.Bullet> / </Styled.Bullet>
       <Styled.Year>
         <time dateTime={String(new Date().getFullYear())}>
           2006–{new Date().getFullYear()}
