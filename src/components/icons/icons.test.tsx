180713f1e87baf36d78383399fb7979e7a325715
Stefan Imhoff <stefan@imhoff.name>
2020-06-09 19:20:03 +0200

    feat(pagination): add pagination component and add it to Haiku detail pages

--- src/components/icons/icons.test.tsx
+++ src/components/icons/icons.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { render } from '../../services/test-utils';
 
-import { Icon as ArrowUp } from './arrow-up.stories';
 import { Icon as ArrowLeft } from './arrow-left.stories';
+import { Icon as ArrowLeftS } from './arrow-left-s.stories';
+import { Icon as ArrowRight } from './arrow-right.stories';
+import { Icon as ArrowRightS } from './arrow-right-s.stories';
+import { Icon as ArrowUp } from './arrow-up.stories';
 import { Icon as Circle } from './circle.stories';
 import { Icon as Download } from './download.stories';
 import { Icon as Information } from './information.stories';
@@ -18,6 +21,24 @@ describe('Icon', () => {
 
   test('ArrowLeft renders correctly', () => {
     const { container } = render(<ArrowLeft />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('ArrowLeftS renders correctly', () => {
+    const { container } = render(<ArrowLeftS />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('ArrowRight renders correctly', () => {
+    const { container } = render(<ArrowRight />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('ArrowRightS renders correctly', () => {
+    const { container } = render(<ArrowRightS />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
