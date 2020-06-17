8174a9c99363e15f59207a5f7c2eb6cca99b0c6b
Stefan Imhoff <stefan@imhoff.name>
2020-06-17 13:35:01 +0200

    refactor(row): replace Flexbox in variable row variants with CSS Grid Layout

--- src/layout/row/row.tsx
+++ src/layout/row/row.tsx
@@ -2,21 +2,28 @@ import * as React from 'react';
 
 import { Styled, variants } from './row.styles';
 
+export type RepeatVariant = 'auto-fit' | 'auto-fill';
 export type RowVariant = keyof typeof variants;
 export type RowProps = {
-  variant?: RowVariant;
-  marginBottom?: boolean;
   debug?: boolean;
+  marginBottom?: boolean;
+  minWidth?: string;
+  repeat?: RepeatVariant;
+  variant?: RowVariant;
 };
 
 export const Row: React.FC<RowProps> = ({
   children,
   debug,
   marginBottom,
+  minWidth = '15rem',
+  repeat = 'auto-fit',
   variant = 'center',
   ...props
 }) => (
   <Styled.Row debug={debug} marginBottom={marginBottom} {...props}>
-    <Styled.Grid variant={variant}>{children}</Styled.Grid>
+    <Styled.Grid minWidth={minWidth} repeat={repeat} variant={variant}>
+      {children}
+    </Styled.Grid>
   </Styled.Row>
 );
