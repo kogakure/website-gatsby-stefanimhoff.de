0a4d68caccac1252d16814f72cc14d1356730935
Stefan Imhoff <stefan@imhoff.name>
2020-06-25 17:24:08 +0200

    chore(variable): make sure variable doesn’t break between lines

--- src/components/variable/variable.styles.tsx
+++ src/components/variable/variable.styles.tsx
@@ -8,6 +8,7 @@ const Variable = styled.var`
   color: var(--colorCodeForeground);
   font-size: ${fontSize[2]};
   padding: ${space[1]} ${space[3]};
+  white-space: nowrap;
 `;
 
 export const Styled = {
