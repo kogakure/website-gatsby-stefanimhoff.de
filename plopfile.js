7374d9ce191729aff61a67241d5f6534576c541c
Stefan Imhoff <stefan@imhoff.name>
2021-03-27 13:28:26 +0100

    chore(plop): update template

--- plopfile.js
+++ plopfile.js
@@ -1,23 +1,57 @@
 /* eslint-disable func-names */
+const moment = require('moment');
+
 const currentDir = process.cwd();
-const currentDate = new Date();
+
+const date = moment().format();
+const year = moment().format('YYYY');
+const week = moment().isoWeek();
 
 const templatePath = 'scripts/templates';
 
 module.exports = function (plop) {
+  const tags = [
+    'book',
+    'code',
+    'design',
+    'download',
+    'film',
+    'health',
+    'minimalism',
+    'personal',
+    'productivit',
+    'publication',
+    'quote',
+    'self-improvement',
+    'software',
+    'tip',
+    'typography',
+  ];
+
   plop.setGenerator('post', {
     description: 'Create a new journal post',
     prompts: [
       {
         type: 'input',
         name: 'title',
-        message: 'Journal Post Title',
+        message: 'Title',
         validate(value) {
           if (/.+/.test(value)) {
             return true;
           }
           return 'Title is required';
         },
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Description',
+      },
+      {
+        type: 'checkbox',
+        name: 'tags',
+        message: 'Tags',
+        choices: tags,
       },
     ],
     actions() {
@@ -26,12 +60,35 @@ module.exports = function (plop) {
       return [
         {
           type: 'addMany',
-          destination: `${currentDir}/content/posts/${currentDate.getFullYear()}/{{dashCase title}}`,
+          destination: `${currentDir}/content/posts/${year}/{{dashCase title}}`,
           base: `${templatePath}/journal`,
           templateFiles: '**/*.txt',
           stripExtensions: ['txt'],
           data: {
-            date: currentDate.toISOString(),
+            date,
+          },
+        },
+      ];
+    },
+  });
+
+  plop.setGenerator('link', {
+    description: 'Create a new link post',
+    prompts: [],
+    actions() {
+      process.chdir(plop.getPlopfilePath());
+
+      return [
+        {
+          type: 'addMany',
+          destination: `${currentDir}/content/posts/${year}/links-${week}-${year}`,
+          base: `${templatePath}/link`,
+          templateFiles: '**/*.txt',
+          stripExtensions: ['txt'],
+          data: {
+            date,
+            week,
+            year,
           },
         },
       ];
